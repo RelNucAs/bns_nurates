b23b4d62cf72866f3a2110af4a960ebf16fb89f7
Surendra Padamata <ssp5361@psu.edu>
2021-10-05 00:14:54 -0400

    create development branch

---
+++ main.cpp
@@ -0,0 +1,2 @@
+// This contains all the main functions which implement the various opactities due to neutral current interactions (elastic scattering)
+// Based on Bruenn(1985) and Burrows et al(2004). List of interactions considered:
