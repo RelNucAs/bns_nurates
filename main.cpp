0ecd3f92f7085cb5e8ec784a91ee470d4a46a5ac
Surendra Padamata <ssp5361@psu.edu>
2022-01-11 15:20:07 -0500

    Jan 11 2022

--- main.cpp
+++ main.cpp
@@ -42,12 +42,23 @@ double Feqnu(double e_nu, double mu_e, double mu_np, double temp)
 return pow(exp((e_nu-mu_e+mu_np)/(kb*temp))+1,-1)  //beta equilibrium assumed (mu_nu = mu_e-mu_np)
 }
 
+double Feqnu_bar(double e_nu_bar, double mu_e, double mu_np, double temp)
+{
+return pow(exp((e_nu_bar+mu_e-mu_np)/(kb*temp))+1,-1)  //beta equilibrium assumed (mu_nu = mu_e-mu_np)
+}
+
 //blocking factor and stimulated absorption
 double B_nu(double e_nu, double mu_e, double mu_np, double temp)
 {
-Bf = (Fn*(1-Fe)*(1-Fp)*(Feqnu-Fnu))/(1-F_tilde_nu)
+Bf = (Fn*(1-Fe)*(1-Fp)*(Feqnu(e_nu, mu_e, mu_np, temp)-Fnu))/(1-F_tilde_nu)
 return Bf
 } 
+
+double B_nu_bar(double e_nu_bar, double mu_e, double mu_np, double temp)
+{
+Bf = (Fn*(1-Fe)*(1-Fp)*(Feqnu_bar(e_nu_bar, mu_e, mu_np, temp)-Fnu))/(1-F_tilde_nu)
+return Bf
+}
 
 //weak magnetism
 double Wm(double e_nu)
@@ -65,23 +76,27 @@ return (1-7.1*e_nu_bar)/n_rm
 //electron neutrino absorption on neutrons
 double nu_n_abs(double rho, double temp, double ye, double e_nu)
 {
-double sigma_nun_abs = sigma_o*((1+3*pow(gA,2))/4)*((e_nu+delta_np)/e_rm)*pow((1-pow((e_rm/(e_nu+delta_np)),2)),0.5)*Wm(e_nu)
+double nb = rho/mn
+double sigma_nun_abs = nb*sigma_o*((1+3*pow(gA,2))/4)*((e_nu+delta_np)/e_rm)*pow((1-pow((e_rm/(e_nu+delta_np)),2)),0.5)*Wm(e_nu)*B_nu(e_nu, mu_e, mu_np, temp)
 return sigma_nun_abs 
 }
 
 //electron neutrino absorption on protonss
 double nu_p_abs(double rho, double temp, double ye, double e_nu_bar)
 {
-double sigma_nup_abs = sigma_o*((1+3*pow(gA,2))/4)*((e_nu_bar-delta_np)/e_rm)*pow((1-pow((e_rm/(e_nu_bar-delta_np)),2)),0.5)*Wm_bar(e_nu_bar)
+double nb = rho/mn
+double sigma_nup_abs = nb*sigma_o*((1+3*pow(gA,2))/4)*((e_nu_bar-delta_np)/e_rm)*pow((1-pow((e_rm/(e_nu_bar-delta_np)),2)),0.5)*Wm_bar(e_nu_bar)*B_nu_bar(e_nu_bar, mu_e, mu_np, temp)
 return sigma_nup_abs
 }
 
 int main (){
 double rho =;
 double temp =;
-double ye=;
-nun = nu_n_abs(rho, temp, ye);
-nup = nu_p_abs(rho, temp, ye);
+double ye =;
+double e_nu =;
+double e_nu_bar = ;
+nun = nu_n_abs(rho, temp, ye, e_nu);
+nup = nu_p_abs(rho, temp, ye, e_nu_bar);
 std::cout << "Neutrino absorption on neutrons for" << "rho =" << rho << "," << "temp =" << temp << "is" << nup <<std:endl;
 std::cout << "Neutrino absorption on protonss for" << "rho =" << rho << "," << "temp =" << temp << "is" << nun <<std:endl;
 }
