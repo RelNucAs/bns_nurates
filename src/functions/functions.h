d4a6f2006f003ec71478b093e3a153a81340d0f4
LeonardoChiesa <leonardo.chiesa@studenti.unitn.it>
2023-06-02 23:41:18 +0200

    Adding implementation of safe exponential

--- src/functions/functions.h
+++ src/functions/functions.h
@@ -5,4 +5,14 @@
 #ifndef BNS_NURATES_SRC_FUNCTIONS_FUNCTIONS_H_
 #define BNS_NURATES_SRC_FUNCTIONS_FUNCTIONS_H_
 
+
+/*===========================================================================*/
+
+// safe_exp.c
+
+// Safe exp function to avoid underflow/overflow
+double SafeExp (const double x);
+
+/*===========================================================================*/
+
 #endif //BNS_NURATES_SRC_FUNCTIONS_FUNCTIONS_H_
---
+++ src/functions/safe_exp.c
@@ -0,0 +1,20 @@
+#include <math.h>
+
+#include "functions.h"
+
+// Cut-off parameter for exponential evaluation
+// Warning: this value was tuned on the previous version of the code
+//          (with C++ math functions) 
+const double fExpUppLim = 700.;          // possible alternative choice: const*DBL_MAX_EXP
+const double fExpLowLim = -fExpUppLim;   //                              const*DBL_MIN_EXP
+
+// Safe exp function to avoid underflow/overflow
+double SafeExp (const double x) {
+	return exp(fmin(fmax(x,-fExpLim),fExpLim));
+}
+
+
+
+
+
+
