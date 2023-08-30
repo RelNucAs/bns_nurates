459262dfd4b51b9652ee4d9c4b2dc49fca7c2883
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2023-08-30 15:14:27 -0400

    some more cleanup and documentation for the 2d integrals

--- src/opacities/m1_opacities.c
+++ src/opacities/m1_opacities.c
@@ -12,28 +12,24 @@
 #include "../integration/integration.h"
 #include "../distribution/distribution.h"
 
-/* Computes the integrand for all double integrals from Leonardo's notes
+/* Computes the integrands from all double integration terms in Leonardo's notes (See Eqn. (50) and (51))
+ * Each computation is perfomed for the 'e' & 'x' type neutrinos, so a total of 6 integrands are computed
  *
- * A total of six integrands are computed
- *
+ * Integrand 1: (4 pi)^2/(hc)^6 nu^3 nubar^2 [R^prod(pair) + R^prod(brem)][1 - g_nubar]
+ * Integrand 2: (1/(c J))(4 pi)^2/(hc)^6 nu^3 nubar^2 g_nu [R^prod(pair) + R^prod(brem)][1 - g_nubar]
+ * Integrand 3: (1/(c J))(4 pi)^2/(hc)^6 nu^3 nubar^2 g_nu [R^ann(pair) + R^ann(brem)] g_nubar
  */
-void M1DoubleIntegrand(double nu, double nubar, GreyOpacityParams *my_grey_opacity_params) {
+void M1DoubleIntegrand(double nu, double nubar, GreyOpacityParams *my_grey_opacity_params, double *result) {
 
   const double four_pi_hc3 = (4. * kPi) / (kH * kH * kH * kClight * kClight * kClight);
-  const double four_pi_hc3_cJ_e = four_pi_hc3 / (kClight * 0.); // @TODO: Add J_e from M1
-  const double four_pi_hc3_cJ_x = four_pi_hc3 / (kClight * 0.); // @TODO: Add J_x from M1
+  const double four_pi_hc3_sqr_cJ_e = four_pi_hc3 / (kClight * 1.); // @TODO: Add J_e from M1
+  const double four_pi_hc3_sqr_cJ_x = four_pi_hc3 / (kClight * 1.); // @TODO: Add J_x from M1
 
   MyEOSParams my_eos_params = my_grey_opacity_params->eos_pars;
 
-  // compute the anti-neutrino distribution function
-  double g_t_nubar = NuFThick(nubar, &my_grey_opacity_params->distr_pars);
-  double g_f_nubar = NuFThin(nubar, &my_grey_opacity_params->distr_pars);
-  double g_nubar = my_grey_opacity_params->distr_pars.w_t * g_t_nubar + my_grey_opacity_params->distr_pars.w_f * g_f_nubar;
-
-  // compute the neutrino distribution function
-  double g_t_nu = NuFThick(nu, &my_grey_opacity_params->distr_pars);
-  double g_f_nu = NuFThin(nu, &my_grey_opacity_params->distr_pars);
-  double g_nu = my_grey_opacity_params->distr_pars.w_t * g_t_nu + my_grey_opacity_params->distr_pars.w_f * g_f_nu;
+  // compute the neutrino & anti-neutrino distribution function
+  double g_nu = TotalNuF(kH * nu, &my_grey_opacity_params->distr_pars);
+  double g_nubar = TotalNuF(kH * nubar, &my_grey_opacity_params->distr_pars);
 
   // compute the pair kernels
   MyKernelParams my_kernel_params;
@@ -54,11 +50,20 @@ void M1DoubleIntegrand(double nu, double nubar, GreyOpacityParams *my_grey_opaci
   double integrand_1_e = four_pi_hc3 * four_pi_hc3 * nu * nu * nu * nubar * nubar * (1. - g_nubar) * (pair_kernels_m1.em_e + brem_kernels_m1.em_e);
   double integrand_1_x = four_pi_hc3 * four_pi_hc3 * nu * nu * nu * nubar * nubar * (1. - g_nubar) * (pair_kernels_m1.em_x + brem_kernels_m1.em_x);
 
-  double integrand_2_e = integrand_1_e * g_nu * four_pi_hc3_cJ_e;
-  double integrand_2_x = integrand_1_x * g_nu * four_pi_hc3_cJ_x;
+  double integrand_2_e = integrand_1_e * g_nu * four_pi_hc3_sqr_cJ_e;
+  double integrand_2_x = integrand_1_x * g_nu * four_pi_hc3_sqr_cJ_x;
 
-  double integrand_3_e = four_pi_hc3_cJ_e * four_pi_hc3 * nu * nu * nu * nubar * nubar * g_nu * g_nubar * (pair_kernels_m1.abs_e + brem_kernels_m1.abs_e);
-  double integrand_3_x = four_pi_hc3_cJ_x * four_pi_hc3 * nu * nu * nu * nubar * nubar * g_nu * g_nubar * (pair_kernels_m1.abs_x + brem_kernels_m1.abs_x);
+  double integrand_3_e = four_pi_hc3_sqr_cJ_e * four_pi_hc3 * nu * nu * nu * nubar * nubar * g_nu * g_nubar * (pair_kernels_m1.abs_e + brem_kernels_m1.abs_e);
+  double integrand_3_x = four_pi_hc3_sqr_cJ_x * four_pi_hc3 * nu * nu * nu * nubar * nubar * g_nu * g_nubar * (pair_kernels_m1.abs_x + brem_kernels_m1.abs_x);
+
+  //double result[6];
+
+  result[0] = integrand_1_e;
+  result[1] = integrand_1_x;
+  result[2] = integrand_2_e;
+  result[3] = integrand_2_x;
+  result[4] = integrand_3_e;
+  result[5] = integrand_3_x;
 
 }
 
