41f77ed15d3856f2f0aa3414b2c4a7eb06c37fd1
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2023-08-30 13:54:33 -0400

    cleanup of double integrals (M1 quantities need to be added)

--- src/opacities/m1_opacities.c
+++ src/opacities/m1_opacities.c
@@ -23,12 +23,21 @@
  */
 void M1DoubleIntegrand(double nu, double nubar, GreyOpacityParams *my_grey_opacity_params) {
 
+  const double four_pi_hc3 = (4. * kPi) / (kH * kH * kH * kClight * kClight * kClight);
+  const double four_pi_hc3_cJ_e = four_pi_hc3 / (kClight * 0.); // @TODO: Add J_e from M1
+  const double four_pi_hc3_cJ_x = four_pi_hc3 / (kClight * 0.); // @TODO: Add J_x from M1
+
   MyEOSParams my_eos_params = my_grey_opacity_params->eos_pars;
 
   // compute the anti-neutrino distribution function
-  double g_t = NuFThick(nubar, &my_grey_opacity_params->distr_pars);
-  double g_f = NuFThin(nubar, &my_grey_opacity_params->distr_pars);
-  double gbar = my_grey_opacity_params->distr_pars.w_t * g_t + my_grey_opacity_params->distr_pars.w_f * g_f;
+  double g_t_nubar = NuFThick(nubar, &my_grey_opacity_params->distr_pars);
+  double g_f_nubar = NuFThin(nubar, &my_grey_opacity_params->distr_pars);
+  double g_nubar = my_grey_opacity_params->distr_pars.w_t * g_t_nubar + my_grey_opacity_params->distr_pars.w_f * g_f_nubar;
+
+  // compute the neutrino distribution function
+  double g_t_nu = NuFThick(nu, &my_grey_opacity_params->distr_pars);
+  double g_f_nu = NuFThin(nu, &my_grey_opacity_params->distr_pars);
+  double g_nu = my_grey_opacity_params->distr_pars.w_t * g_t_nu + my_grey_opacity_params->distr_pars.w_f * g_f_nu;
 
   // compute the pair kernels
   MyKernelParams my_kernel_params;
@@ -39,22 +48,21 @@ void M1DoubleIntegrand(double nu, double nubar, GreyOpacityParams *my_grey_opaci
   my_kernel_params.pair_kernel_params.lmax = 0;
   my_kernel_params.pair_kernel_params.mu = 1.;
   my_kernel_params.pair_kernel_params.mu_prime = 1.;
-
   MyOpacityQuantity pair_kernels_m1 = PairKernelsM1(&my_eos_params, &my_kernel_params.pair_kernel_params);
 
-  // compute the bremsstrahlung kernels
+  // compute the bremsstrahlung kernels (TODO: Leonardo, check this!)
+  my_kernel_params.brem_kernel_params.omega = kH * nu;
+  my_kernel_params.brem_kernel_params.omega_prime = kH * nubar;
+  MyOpacityQuantity brem_kernels_m1 = BremKernels(&my_kernel_params.brem_kernel_params, &my_eos_params);
 
-  double integrand_1_e = nubar * nubar * (1. - gbar) * (pair_kernels_m1.em_e + 0.);
-  double integrand_1_x = nubar * nubar * (1. - gbar) * (pair_kernels_m1.em_x + 0.);
+  double integrand_1_e = four_pi_hc3 * four_pi_hc3 * nu * nu * nu * nubar * nubar * (1. - g_nubar) * (pair_kernels_m1.em_e + brem_kernels_m1.em_e);
+  double integrand_1_x = four_pi_hc3 * four_pi_hc3 * nu * nu * nu * nubar * nubar * (1. - g_nubar) * (pair_kernels_m1.em_x + brem_kernels_m1.em_x);
 
-  double integrand_2_e = nubar * nubar * gbar * (pair_kernels_m1.abs_e + 0.);
-  double integrand_2_x = nubar * nubar * gbar * (pair_kernels_m1.abs_x + 0.);
+  double integrand_2_e = integrand_1_e * g_nu * four_pi_hc3_cJ_e;
+  double integrand_2_x = integrand_1_x * g_nu * four_pi_hc3_cJ_x;
 
-  double integrand_eta_e = nu * nu * nu * integrand_1_e;
-  double integrand_eta_x = nu * nu * nu * integrand_1_x;
-
-  double integrand_kappa_a_e = integrand_eta_e * gbar + nu * nu * nu * nubar * nubar * (pair_kernels_m1.abs_e + 0.);
-  double integrand_kappa_a_x = integrand_eta_x * gbar + + nu * nu * nu * nubar * nubar * (pair_kernels_m1.abs_x + 0.);
+  double integrand_3_e = four_pi_hc3_cJ_e * four_pi_hc3 * nu * nu * nu * nubar * nubar * g_nu * g_nubar * (pair_kernels_m1.abs_e + brem_kernels_m1.abs_e);
+  double integrand_3_x = four_pi_hc3_cJ_x * four_pi_hc3 * nu * nu * nu * nubar * nubar * g_nu * g_nubar * (pair_kernels_m1.abs_x + brem_kernels_m1.abs_x);
 
 }
 
