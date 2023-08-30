103b3460cbf957d4414ee653d84fe774b4b658f7
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2023-08-30 12:58:02 -0400

    adding double integrands with pair. wip.

--- src/opacities/m1_opacities.c
+++ src/opacities/m1_opacities.c
@@ -21,15 +21,28 @@
  * Returns two doubles, one for electron neutrino and another for mu/tau neutrino
  *
  */
-MyOpacityQuantity M1IntegrandNubar(double nubar, MyEOSParams *my_eos_params, MyKernelParams *my_kernel_params, GreyOpacityParams *my_grey_opacity_params) {
+void M1DoubleIntegrand(double nu, double nubar, GreyOpacityParams *my_grey_opacity_params) {
+
+  MyEOSParams my_eos_params = my_grey_opacity_params->eos_pars;
 
   // compute the anti-neutrino distribution function
-  double g_t = NuFThick(kH * nubar, &my_grey_opacity_params->distr_pars);
-  double g_f = NuFThin(kH * nubar, &my_grey_opacity_params->distr_pars);
+  double g_t = NuFThick(nubar, &my_grey_opacity_params->distr_pars);
+  double g_f = NuFThin(nubar, &my_grey_opacity_params->distr_pars);
   double gbar = my_grey_opacity_params->distr_pars.w_t * g_t + my_grey_opacity_params->distr_pars.w_f * g_f;
 
   // compute the pair kernels
-  MyOpacityQuantity pair_kernels_m1 = PairKernelsM1(my_eos_params, &my_kernel_params->pair_kernel_params);
+  MyKernelParams my_kernel_params;
+  my_kernel_params.pair_kernel_params.omega = kH * nu;
+  my_kernel_params.pair_kernel_params.omega_prime = kH * nubar;
+  my_kernel_params.pair_kernel_params.cos_theta = 1.;
+  my_kernel_params.pair_kernel_params.filter = 0.;
+  my_kernel_params.pair_kernel_params.lmax = 0;
+  my_kernel_params.pair_kernel_params.mu = 1.;
+  my_kernel_params.pair_kernel_params.mu_prime = 1.;
+
+  MyOpacityQuantity pair_kernels_m1 = PairKernelsM1(&my_eos_params, &my_kernel_params.pair_kernel_params);
+
+  // compute the bremsstrahlung kernels
 
   double integrand_1_e = nubar * nubar * (1. - gbar) * (pair_kernels_m1.em_e + 0.);
   double integrand_1_x = nubar * nubar * (1. - gbar) * (pair_kernels_m1.em_x + 0.);
@@ -37,23 +50,19 @@ MyOpacityQuantity M1IntegrandNubar(double nubar, MyEOSParams *my_eos_params, MyK
   double integrand_2_e = nubar * nubar * gbar * (pair_kernels_m1.abs_e + 0.);
   double integrand_2_x = nubar * nubar * gbar * (pair_kernels_m1.abs_x + 0.);
 
-  MyOpacityQuantity result = {.em_e = integrand_1_e, .em_x = integrand_1_x,
-                              .abs_e = integrand_2_e, .abs_x = integrand_2_x};
+  double integrand_eta_e = nu * nu * nu * integrand_1_e;
+  double integrand_eta_x = nu * nu * nu * integrand_1_x;
 
-  return result;
-}
-
-void M1Integrand2() {
+  double integrand_kappa_a_e = integrand_eta_e * gbar + nu * nu * nu * nubar * nubar * (pair_kernels_m1.abs_e + 0.);
+  double integrand_kappa_a_x = integrand_eta_x * gbar + + nu * nu * nu * nubar * nubar * (pair_kernels_m1.abs_x + 0.);
 
 }
 
-void M1Integrand3() {
+void M1SingleIntegrand(double nu, GreyOpacityParams *my_grey_opacity_params) {
 
 }
 
-void M1Integrand4() {
 
-}
 /* Integrand for the M1 opacities
  *
  */
