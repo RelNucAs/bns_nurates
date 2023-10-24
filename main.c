552cd65dd972313d95cc2273ddf5b5f1e7a8b4fb
Maitraya Bhattacharyya <maitraya.linux@gmail.com>
2023-10-24 19:20:00 -0400

    cmake library wip support

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
-#include <math.h>
+//#include <math.h>
 #include <stdio.h>
-#include "src/constants.h"
-#include "src/bns_nurates.h"
-#include "src/integration/integration.h"
-#include "src/opacities/kernels/kernels.h"
-#include "src/functions/functions.h"
-#include "src/opacities/opacities.h"
+//#include "src/constants.h"
+//#include "src/bns_nurates.h"
+//#include "src/integration/integration.h"
+//#include "src/opacities/kernels/kernels.h"
+//#include "src/functions/functions.h"
+//#include "src/opacities/opacities.h"
 
 int main() {
 
---
+++ src/libnurates.h
@@ -0,0 +1,14 @@
+#ifndef BNS_NURATES_SRC_LIBNURATES_H_
+#define BNS_NURATES_SRC_LIBNURATES_H_
+
+#include "bns_nurates.h"
+#include "constants.h"
+#include "distribution/distribution.h"
+#include "eos/eos.h"
+#include "functions/functions.h"
+#include "integration/integration.h"
+#include "opacities/kernels/kernels.h"
+#include "opacities/weak_magnetism/weak_magnetism.h"
+#include "output/outputs.h"
+
+#endif //BNS_NURATES_SRC_LIBNURATES_H_
